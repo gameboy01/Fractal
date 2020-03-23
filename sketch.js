5408c47353c30850f79e0faa81fa97fec81d8464
Vishaal Varshan <vishaalgameoby@gmail.com>
2020-03-24 00:33:54 +0530

    Files added

--- sketch.js
+++ sketch.js
@@ -1,9 +1,35 @@
-
+var angle = 0;
+var bangle;
+var slider;
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	createCanvas(800, 800);
+	slider = createSlider(0, TWO_PI, PI / 4, 0.01);
+	bslider = createSlider(0, 1, .25, .01);
 }
 
 function draw() {
+	background(51)
+	angle = slider.value();
+	bangle = bslider.value();
+	stroke(255);
+	translate(400, height);
+	branch(200);
+}
 
+function branch(len) {
+	line(0, 0, 0, - len);
+	translate(0, -len);
+	if (len > 4) {
+		push();
+		rotate(angle);
+		branch(len * bangle);
+		pop();
+		push();
+		rotate(-angle);
+		branch(len * bangle);
+		pop();
+	}
+
+	// line(0, 0, 0, - len*.67);
 }
